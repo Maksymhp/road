ece4925fcfe948b3ddae451f1ef23009d9e2844a
Maksymhp <111457593+Maksymhp@users.noreply.github.com>
2025-02-14 00:06:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ const  menu = document.querySelector ('.burger-info');
 menuBtn.addEventListener ('click' , () => {
     menu.classList.toggle('burger-info--active');
 });
+
+menuBtn.addEventListener ('click' , () => {
+    menu.classList.toggle('burger-info--active');
+});
+
+document.getElementById('open-modal-btn').addEventListener('click',function(){
+    document.getElementById('myModal').classList.add('open')
+})
+document.getElementById('close-my-modal-btn').addEventListener('click',function(){
+    document.getElementById('myModal').classList.remove('open')
+})
+
+document.getElementById('open-modal-btn2').addEventListener('click',function(){
+    document.getElementById('myModal2').classList.add('open')
+})
+document.getElementById('close-my-modal-btn2').addEventListener('click',function(){
+    document.getElementById('myModal2').classList.remove('open')
+})
+document.getElementById('open-modal-btn1').addEventListener('click',function(){
+    document.getElementById('myModal1').classList.add('open')
+})
+document.getElementById('close-my-modal-btn1').addEventListener('click',function(){
+    document.getElementById('myModal1').classList.remove('open')
+})
